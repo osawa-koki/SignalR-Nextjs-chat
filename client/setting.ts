e1c3a0cc3dfcde4da5d1cfc50858c0951ca9fe47
osawa-koki <github@osawa.dev>
2023-03-06 13:03:41 +0900

    小さな修正。

--- client/setting.ts
+++ client/setting.ts
@@ -1,11 +1,11 @@
-import Env from './next.config.js';
+import Env from '';
 const isProd = process.env.NODE_ENV === 'production';
 
 const setting = {
   isProd,
   basePath: Env.basePath,
-  apiPath: isProd ? '' : 'http://localhost:8080',
-  title: '🐸 Next.js Template 🐸',
+  apiPath: isProd ? '' : 'http://localhost:8000',
+  title: '🐱 SignalR-Nextjs-chat 🐱',
 };
 
 export default setting;
